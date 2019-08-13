0ea9d2022973f4e7cc609785fb45d930b52678ec
seonggukchoi <mrmilkgun@gmail.com>
2019-08-13 11:34:30 +0900

    --wip-- [skip ci]

--- src/projects/project/project.component.vue
+++ src/projects/project/project.component.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="project">
   <v-card>
-    <v-img
+    <!-- <v-img
       lazy-src="@/assets/logo.png"
       src="@/assets/logo.png"
       position="center"
       gradient="to top, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, .1) 20%, rgba(0, 0, 0, 0) 100%"
       :transition="false"
-      max-height="300px" />
+      max-height="300px" /> -->
     <v-card-title class="title">{{ project.title }}</v-card-title>
     <v-card-text class="caption">{{ project.description }}</v-card-text>
     <v-card-actions>
--- src/projects/projects.store.ts
+++ src/projects/projects.store.ts
@@ -1,5 +1,34 @@
 import { ActionContext } from 'vuex';
 
+const data = {
+  projects: [
+    {
+      id: 1,
+      title: 'Seongg.uk',
+      description: 'Personal Projects',
+      image: '',
+    },
+    {
+      id: 2,
+      title: 'ONDA Plus',
+      description: 'ONDA Projects',
+      image: '',
+    },
+    {
+      id: 3,
+      title: 'ONDA Hotel Extranet',
+      description: 'ONDA Projects',
+      image: '',
+    },
+    {
+      id: 4,
+      title: 'ONDA Wave',
+      description: 'ONDA Projects',
+      image: '',
+    },
+  ],
+};
+
 const state = {
   projects: [],
 } as Projects.IProjectsState;
@@ -19,24 +48,11 @@ const mutations = {
 const actions = {
   async fetchProjects(storeContext: ActionContext<unknown, unknown>) {
     return new Promise((resolve, reject) => {
-      const projects = [
-        { id: 1, title: 'Seongg.uk', description: 'Personal Projects', image: '' },
-        { id: 2, title: 'ONDA Plus', description: 'ONDA Projects', image: '' },
-        { id: 3, title: 'ONDA Hotel Extranet', description: 'ONDA Projects', image: '' },
-        { id: 4, title: 'ONDA Wave', description: 'ONDA Projects', image: '' },
-        { id: 5, title: 'Seongg.uk', description: 'Personal Projects', image: '' },
-        { id: 6, title: 'ONDA Plus', description: 'ONDA Projects', image: '' },
-        { id: 7, title: 'ONDA Hotel Extranet', description: 'ONDA Projects', image: '' },
-        { id: 8, title: 'ONDA Wave', description: 'ONDA Projects', image: '' },
-        { id: 9, title: 'Seongg.uk', description: 'Personal Projects', image: '' },
-        { id: 10, title: 'ONDA Plus', description: 'ONDA Projects', image: '' },
-        { id: 11, title: 'ONDA Hotel Extranet', description: 'ONDA Projects', image: '' },
-        { id: 12, title: 'ONDA Wave', description: 'ONDA Projects', image: '' },
-      ];
+      const projects = data.projects;
 
       storeContext.commit('setProjects', projects);
 
-      setTimeout(() => resolve(projects), 2000);
+      resolve(projects);
     });
   },
 };
